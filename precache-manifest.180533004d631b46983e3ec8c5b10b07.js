d916d7c3178bdf539bec6ad07d55e1507cee3e78
Ali Tanrıseven <95570387+alitnrsvn1@users.noreply.github.com>
2024-04-01 21:53:13 +0300

    Updates

---
+++ precache-manifest.180533004d631b46983e3ec8c5b10b07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd227ac662a0113d1fa2a195b283094",
+    "url": "/emojiarama/index.html"
+  },
+  {
+    "revision": "022c22ceed0749b0ae55",
+    "url": "/emojiarama/static/css/2.5510c45e.chunk.css"
+  },
+  {
+    "revision": "55c1854491fc4a720e71",
+    "url": "/emojiarama/static/css/main.65605343.chunk.css"
+  },
+  {
+    "revision": "022c22ceed0749b0ae55",
+    "url": "/emojiarama/static/js/2.9f764114.chunk.js"
+  },
+  {
+    "revision": "2ce1509b9f5d601b23dc0321a205137e",
+    "url": "/emojiarama/static/js/2.9f764114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55c1854491fc4a720e71",
+    "url": "/emojiarama/static/js/main.ac0a1072.chunk.js"
+  },
+  {
+    "revision": "6d4ffa1ad2dfad280352",
+    "url": "/emojiarama/static/js/runtime-main.71c9c30c.js"
+  }
+]);
